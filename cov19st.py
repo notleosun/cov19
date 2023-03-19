c2ba391aa8e5ce94bd2ff6a7bbc97e25d9f05ec7
leo “ning” sun <88621543+notleosun@users.noreply.github.com>
2023-03-19 12:21:42 +0000

    Update cov19st.py

--- cov19st.py
+++ cov19st.py
@@ -7,7 +7,7 @@ from plotly.subplots import make_subplots
 import streamlit as st
 
 with st.sidebar: 
-	selected = st.option_menu(
+	selected = option_menu(
 		menu_title = 'Navigation Pane',
 		options = ['Abstract', 'Background Information', 'Data Cleaning','Exploratory Analysis','Data Analysis', 'Conclusion', 'Bibliography'],
 		menu_icon = 'arrow-down-right-circle-fill',
