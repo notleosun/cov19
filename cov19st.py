522eb9230b237fc526dd1a548b770a7a74e4a251
leo “ning” sun <88621543+notleosun@users.noreply.github.com>
2023-03-19 13:31:10 +0000

    Update cov19st.py

--- cov19st.py
+++ cov19st.py
@@ -32,9 +32,71 @@ for c in cov19.columns:
     
 cov19.columns = new_columns
 	""")
+	st.subheader("Assigning Continents ")
 	continent_dict = pd.read_csv("continents2.csv")
 	st.code("""
 countrycode = pd.read_csv("continents2.csv")
 countrycode
 	""")
 	continent_dict
+	st.code("""
+country_dict = {
+                    " Azerbaijan":"Azerbaijan",
+                    "Bahamas  The":"Bahamas",
+                    "Congo (Brazzaville)":"Republic of the Congo",
+                    "Congo (Kinshasa)":"Republic of the Congo",
+                    "Gambia  The":"Gambia",
+                    "Guinea-Bissau":"Guinea Bissau",
+                    "Mainland China":"China",
+                    "occupied Palestinian territory":"Palestine",
+                    "('St. Martin' )":"St. Martin",
+                    "The Bahamas":"Bahamas",
+                    "The Gambia":"Gambia"
+                   }
+cov19["Country"] = cov19.Country.replace(country_dict)	
+	""")
+	st.subheading("Assigning Regions ")
+	st.code("""
+region_dict = {country:region for country, region in zip(countrycode["name"], countrycode["sub-region"])}
+region_dict.update({
+                    " Azerbaijan":"Western Asia",
+                    "Bahamas  The":"Latin America and the Caribbean",
+                    "Bosnia and Herzegovina":"Southern Europe",
+                    "Brunei":"South-eastern Asia",
+                    "Burma":"South-eastern Asia",
+                    "Cape Verde":"Sub-Saharan Africa",
+                    "Channel Islands":"Northern Europe",
+                    "Congo (Brazzaville)":"Sub-Saharan Africa",
+                    "Congo (Kinshasa)":"Sub-Saharan Africa",
+                    "Curacao":"Latin America and the Caribbean",
+                    "Diamond Princess":"Northern Europe",
+                    "East Timor":"South-eastern Asia",
+                    "Gambia  The":"Sub-Saharan Africa",
+                    "Guinea-Bissau":"Sub-Saharan Africa",
+                    "Ivory Coast":"Sub-Saharan Africa",
+                    "Kosovo":"Southern Europe",
+                    "Republic of the Congo":"Sub-Saharan Africa",
+                    "Macau":"Eastern Asia",
+                    "Mainland China":"Eastern Asia",
+                    "MS Zaandam":"Western Europe",
+                    "Northern Ireland":"Northern Europe",
+                    "North Ireland":"Northern Europe",
+                    "North Macedonia":"Southern Europe",
+                    "occupied Palestinian territory":"Western Asia",
+                    "Others":"Northern Europe",
+                    "Palestine":"Western Asia",
+                    "Republic of Ireland":"Northern Europe",
+                    "Reunion":"Sub-Saharan Africa",
+                    "Saint Barthelemy":"Latin America and the Caribbean",
+                    "('St. Martin' )":"Latin America and the Caribbean",
+                    "St. Martin":"Latin America and the Caribbean",  
+                    "UK":"Northern Europe",
+                    "US":"Northern America",
+                    "The Bahamas":"Latin America and the Caribbean",
+                    "The Gambia":"Sub-Saharan Africa",
+                    "Vatican City":"Southern Europe",
+                    "West Bank and Gaza":"Western Asia"
+                   })
+                   
+cov19["Region"] = cov19.Country.replace(region_dict)
+	""")
