9fa8a8f06e6aa7f6a810780b2d598014cd37c50c
leo “ning” sun <88621543+notleosun@users.noreply.github.com>
2023-03-19 09:55:44 +0000

    Update cov19st.py

--- cov19st.py
+++ cov19st.py
@@ -6,3 +6,13 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import streamlit as st
 from streamlit_option_menu import option_menu
+
+with st.sidebar: 
+	selected = option_menu(
+		menu_title = 'Navigation Pane',
+		options = ['Abstract', 'Background Information', 'Data Cleaning','Exploratory Analysis','Data Analysis', 'Conclusion', 'Bibliography'],
+		menu_icon = 'arrow-down-right-circle-fill',
+		icons = ['bookmark-check', 'book', 'box', 'map', 'boxes', 'bar-chart', 
+		'check2-circle'],
+		default_index = 0,
+		)
