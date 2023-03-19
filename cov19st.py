fabd737dc8277dba031546039b3d2e1fe5005528
leo “ning” sun <88621543+notleosun@users.noreply.github.com>
2023-03-19 09:59:01 +0000

    Update cov19st.py

--- cov19st.py
+++ cov19st.py
@@ -5,7 +5,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import streamlit as st
-from streamlit_option_menu import option_menu
 
 with st.sidebar: 
 	selected = option_menu(
